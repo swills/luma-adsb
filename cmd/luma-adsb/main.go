93963d2f3019485c0b977deb882c0b4989a65eb3
Steve Wills <steve@mouf.net>
2025-10-25 16:05:29 -0400

    refactor

--- cmd/luma-adsb/main.go
+++ cmd/luma-adsb/main.go
@@ -9,10 +9,15 @@ import (
 	"image/color"
 	"image/draw"
 	"io"
+	"log/slog"
+	"math"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
+	"github.com/jftuga/geodist"
 	"github.com/waxdred/go-i2c-oled"
 	"github.com/waxdred/go-i2c-oled/ssd1306"
 	"golang.org/x/image/font"
@@ -70,14 +75,44 @@ func main() {
 		_ = oled.Close()
 	}(oled)
 
+	var initError bool
+
 	host := os.Getenv("ADSBFEED_HOST")
 	if host == "" {
 		fmt.Printf("ADSBFEED_HOST not set (probably YOURHOSTNAME)\n")
-
-		return
+		initError = true
 	}
 
-	stats := &stage2stats{
+	myLatStr := os.Getenv("ADSBFEED_LAT")
+	if myLatStr == "" {
+		fmt.Printf("ADSBFEED_LAT not set (probably something like \"12.345678\")\n")
+		initError = true
+	}
+
+	myLatFloat, err := strconv.ParseFloat(myLatStr, 64)
+	if err != nil {
+		fmt.Printf("error parsing ADSBFEED_LAT: %s\n", err)
+
+		initError = true
+	}
+
+	myLonStr := os.Getenv("ADSBFEED_LON")
+	if myLonStr == "" {
+		fmt.Printf("ADSBFEED_LON not set (probably something lke \"12.345678\")\n")
+		initError = true
+	}
+
+	myLonFloat, err := strconv.ParseFloat(myLonStr, 64)
+	if err != nil {
+		fmt.Printf("error parsing ADSBFEED_LON: %s\n", err)
+		initError = true
+	}
+
+	if initError {
+		os.Exit(1)
+	}
+
+	stats := stage2stats{
 		Pps:         0,
 		Mps:         0,
 		Uptime:      0,
@@ -85,37 +120,107 @@ func main() {
 		TotalPlanes: 0,
 	}
 
-	displayTicker := time.NewTicker(1 * time.Second)
-	stage2Ticker := time.NewTicker(5 * time.Second)
-	//aircraftDataTicker := time.NewTicker(5 * time.Second)
+	myADSBData := ADSBData{
+		Planes: make([]Aircraft, 0),
+	}
 
-	var err error
+	displayTicker := time.NewTicker(500 * time.Millisecond) // faster than 300ms causes issues
+	stage2Ticker := time.NewTicker(1 * time.Second)
+	aircraftDataTicker := time.NewTicker(1 * time.Second)
 
 	for {
 		select {
 		case <-stage2Ticker.C:
-			stats, err = getStage2Stats(host)
-			if err != nil {
-				fmt.Printf("error: %s\n", err)
-
-				continue
-			}
+			go getAndUpdateStats(&stats, host)
+		case <-aircraftDataTicker.C:
+			go getAndUpdateADSBData(&myADSBData, host)
 		case <-displayTicker.C:
-			now := time.Now()
-			dispLines := []string{
-				fmt.Sprintf("Time: %s", now.Format("15:04:05")),
-				fmt.Sprintf("Planes: %d", stats.Planes),
-				fmt.Sprintf("Planes Today: %d", stats.TotalPlanes),
-				"",
-				"",
-			}
-
-			err = updateDisplayLines(dispLines, oled)
-			if err != nil {
-				fmt.Printf("error updating display: %s", err)
-			}
+			go buildDisplayInfoAndUpdateDisplay(&myADSBData, &stats, myLatFloat, myLonFloat, oled)
 		}
+	}
+}
 
+func getAndUpdateADSBData(data *ADSBData, host string) {
+	newADSBData, err := getADSBData(host)
+	if err != nil {
+		fmt.Printf("error getting adsb data: %s", err)
+	} else {
+		*data = *newADSBData
+	}
+}
+
+func getADSBData(host string) (*ADSBData, error) {
+	var err error
+
+	var req *http.Request
+
+	var res *http.Response
+
+	url := fmt.Sprintf("http://%s:8080/data/aircraft.json", host)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*500)
+	defer cancel()
+
+	req, err = http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return &ADSBData{}, fmt.Errorf("error creating http req: %w", err)
+	}
+
+	res, err = http.DefaultClient.Do(req)
+	if err != nil {
+		return &ADSBData{}, fmt.Errorf("error making http request: %w", err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		slog.Error("bad status making request", "status", res.StatusCode)
+		return &ADSBData{}, fmt.Errorf("bad status making request: %w", err)
+	}
+
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(res.Body)
+
+	var body []byte
+
+	body, err = io.ReadAll(res.Body)
+	if err != nil {
+		return &ADSBData{}, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	var myADSBData ADSBData
+
+	err = json.Unmarshal(body, &myADSBData)
+	if err != nil {
+		return &ADSBData{}, fmt.Errorf("failed unmarshalling: %w", err)
+	}
+
+	return &myADSBData, nil
+}
+
+func findClosest(myADSBData ADSBData, myLatFloat float64, myLonFloat float64) (Aircraft, float64) {
+	myLoc := geodist.Coord{Lat: myLatFloat, Lon: myLonFloat}
+
+	var closestDist = math.MaxFloat64
+	var closestPlane Aircraft
+
+	for _, flight := range myADSBData.Planes {
+		planeLoc := geodist.Coord{Lat: flight.Latitude, Lon: flight.Longitude}
+		distanceMiles, _ := geodist.HaversineDistance(myLoc, planeLoc)
+		if distanceMiles < closestDist {
+			closestDist = distanceMiles
+			closestPlane = flight
+		}
+	}
+
+	return closestPlane, closestDist
+}
+
+func getAndUpdateStats(stats *stage2stats, host string) {
+	newStats, err := getStage2Stats(host)
+	if err != nil {
+		fmt.Printf("error: %s\n", err)
+	} else {
+		*stats = *newStats
 	}
 }
 
@@ -126,7 +231,7 @@ func getStage2Stats(host string) (*stage2stats, error) {
 
 	var res *http.Response
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*500)
 	defer cancel()
 
 	url := fmt.Sprintf("http://%s/api/stage2_stats", host)
@@ -170,6 +275,45 @@ func getStage2Stats(host string) (*stage2stats, error) {
 	return &foundStage2Stats[0], nil
 }
 
+func buildDisplayInfoAndUpdateDisplay(myADSBData *ADSBData, stats *stage2stats, myLatFloat float64, myLonFloat float64, oled *goi2coled.I2c) {
+	var err error
+
+	var numPlanes int
+
+	if len(myADSBData.Planes) > 0 {
+		numPlanes = len(myADSBData.Planes)
+	} else {
+		numPlanes = stats.Planes
+	}
+
+	dispLines := []string{
+		fmt.Sprintf("%s P: %d", time.Now().Format("15:04:05"), numPlanes),
+	}
+
+	if len(myADSBData.Planes) > 0 {
+		closestPlane, dist := findClosest(*myADSBData, myLatFloat, myLonFloat)
+		closest := strings.TrimSpace(closestPlane.CallSign)
+		if closest == "" {
+			closest = "none"
+		}
+		dispLines = append(dispLines, fmt.Sprintf("C: %s (%s)", closest, closestPlane.Hex))
+		if closestPlane.Category != "" {
+			dispLines = append(dispLines, fmt.Sprintf("D: %2.2f (%s)", dist, closestPlane.Category))
+		} else {
+			dispLines = append(dispLines, fmt.Sprintf("D: %2.2f", dist))
+		}
+	}
+
+	err = updateDisplayLines(dispLines, oled)
+	if err != nil {
+		fmt.Printf("error updating display: %s", err)
+	}
+}
+
+func clearDisplay(oled *goi2coled.I2c) {
+	draw.Draw(oled.Img, oled.Img.Bounds(), &image.Uniform{C: color.Black}, image.Point{}, draw.Src)
+}
+
 func updateDisplayLines(dispLines displayLines, oled *goi2coled.I2c) error {
 	h := basicfont.Face7x13.Metrics().Height
 	var err error
@@ -188,7 +332,7 @@ func updateDisplayLines(dispLines displayLines, oled *goi2coled.I2c) error {
 		Dot:  point,
 	}
 
-	draw.Draw(oled.Img, oled.Img.Bounds(), &image.Uniform{C: color.Black}, image.Point{}, draw.Src)
+	clearDisplay(oled)
 
 	for i, line := range dispLines {
 		if line == "" || i > 5 {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/swills/luma-adsb
 go 1.24.9
 
 require (
+	github.com/jftuga/geodist v1.0.0
 	github.com/waxdred/go-i2c-oled v1.0.1
 	golang.org/x/image v0.32.0
 )
